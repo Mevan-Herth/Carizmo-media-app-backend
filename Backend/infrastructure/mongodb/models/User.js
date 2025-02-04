ae9b1657e523c12a3a33df56704f486ed0729309
yogisha mevan <yogisha.mevan4321@gmail.com>
2025-02-03 21:06:45 -0500

    Connect app to backend, implement user registration, and create user schema

---
+++ Backend/infrastructure/mongodb/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'] // Basic email validation
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+    coverPicture: {
+        type: String,
+        default: ""
+    },
+    followers: {
+        type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+        default: []
+    },
+    following: {
+        type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+        default: []
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true } // Adds createdAt and updatedAt
+); 
+module.exports = mongoose.model('User', UserSchema);
