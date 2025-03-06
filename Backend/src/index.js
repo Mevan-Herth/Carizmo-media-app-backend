ed1247eefc95b2ce2d419405585dbbc39f189527
Omareo518 <omarabdullahi518@gmail.com>
2025-03-06 14:46:25 -0500

    Crud and Schema Comments

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const userRoutes = require('./interfaces/routes/userRoutes');
 const userProfileRoutes = require('./interfaces/routes/userProfileRoutes');
+const commentRoutes = require('./interfaces/routes/CommentRoutes'); 
 const { connectDB, env, serverConfig, logger } = require('./config');
 const {} = require('./application/userCases');
 const {authMiddleware} = require('../src/shared/middlewares/authMiddleware');
@@ -23,6 +24,10 @@ app.use('/api/users', userRoutes);
 //protected routes
 app.use(authMiddleware);
 app.use('/api/users', userProfileRoutes);
+
+
+// Comment routes
+app.use('/api/comments', commentRoutes); 
 
 // Start the server
 app.listen(serverConfig.port, () => {
