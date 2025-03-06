f53c30afc81a4fedfbf3a70627e40ab1b7f6a4d8
AstronautGuy04 <pu040200@gmail.com>
2025-03-06 17:43:33 -0500

    feature/GROUP-21

---
+++ Backend/src/infrastructure/database/models/CommentSchema.js
@@ -0,0 +1,30 @@
+// src/infrastructure/database/models/commentSchema.js
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
