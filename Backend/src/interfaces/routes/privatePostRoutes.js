a5e07043a074d948194c11e5e37202e8f5337cc7
Mokey427 <148743040+Mokey427@users.noreply.github.com>
2025-03-27 01:38:50 -0400

    finsihed
    added image uplaoding in posts

--- Backend/src/interfaces/routes/privatePostRoutes.js
+++ Backend/src/interfaces/routes/privatePostRoutes.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const router = express.Router();  
+<<<<<<< Updated upstream:Backend/src/interfaces/routes/privatePostRoutes.js
 const Post = require("../controllers/Posts")
+=======
+const Post = require("../../../infrastructure/Posts")
+const postDependencies = require("../../../infrastructure/dependencies/dependencies")
+const uploadImage = require("../../../shared/middlewares/postImageMiddleware")
+>>>>>>> Stashed changes:Backend/src/ports/rest/routes/privatePostRoutes.js
 
 
-router.post("/add-post", async (req, res) => {
+router.post("/add-post",uploadImage, async (req, res) => {
   const {title, content} = req.body
   try {
+<<<<<<< Updated upstream:Backend/src/interfaces/routes/privatePostRoutes.js
     const result = await Post.addPost(title,content,req.userId)
+=======
+    const result = await Post.addPost(postDependencies)(title,content,req.userId,req.files)
+>>>>>>> Stashed changes:Backend/src/ports/rest/routes/privatePostRoutes.js
     return res.status(200).json({message:`Post added ${result}`})
     }
   catch(error) {return res.status(400).json({message:`${error}`})}
